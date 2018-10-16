c7774012ef8a6bce9c64ab2b820f0f40464425be
Marcodt21 <42016727+Marcodt21@users.noreply.github.com>
2018-10-16 19:02:45 -0400

    fff

--- Laboratorio2 pregunta3.c
+++ Laboratorio2 pregunta3.c
@@ -8,16 +8,17 @@ return x*x - 2;
 ///Pre: f(a)*f(b) < 0, a < b, er > 0 cecano a CERO
 float biseccion( float a, float b, float er){
 float m=(b+a)/2;
-while (f(m)>=0+er||f(m)<=0+er){
-    m = (b+a)/2;
-    if(fabs(f(a))<fabs(f(b)))b=m;
-    else a=m;
+while (f(m)<=0+er||f(m)>=0-er){
+        if(f(m)<0)a=m;
+        else b=m;
+        m=(a+b)/2;
+        printf("%f",m);
 }
 return m;
 }
 int main(){
 float a = 1.0, b = 2.0; //Cambie el intervalo de búsqueda ...??
-float r = biseccion(a,b,.000001);
+float r = biseccion(a,b,.02);
 printf("\nPrueba del Metodo de Biseccion:\n");
 printf("\n%9.6f es una raiz de la FUNCION f.",r);
 return 0;
