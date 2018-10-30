b3ceb73919bc27fe1adad370817187ae1b5d495c
Marcodt21 <42016727+Marcodt21@users.noreply.github.com>
2018-10-30 16:25:36 -0400

    lqb3

---
+++ LabListaA.c
@@ -0,0 +1,239 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo{
+    int val;
+    struct nodo *sig;
+}Nodo;
+typedef Nodo* Lista;
+
+Lista NewList(){
+    return NULL;
+}
+void insertCab(int x, Lista*L){
+    Nodo*q=malloc(sizeof(Nodo));
+    q->val=x;
+    q->sig=*L;
+    *L=q;
+}
+int esVacia(Lista L){
+    return L==NULL;
+}
+int firstInList(Lista L){
+    return L->val;
+}
+int estaEn(int x, Lista L){
+    Nodo *q=L;
+    while(q!=NULL && q->val!=x)q=q->sig;
+    return q!=NULL;
+}
+void escribeLista(char name[], Lista L){
+    printf("\n%s=[",name);
+    if(L==NULL)printf("]");
+    else{
+        Nodo *q=L;
+        while(q->sig!=NULL){
+                printf("%d ",q->val);
+                q=q->sig;
+        }
+        printf("%d]",q->val);
+    }
+}
+void insertT(int x,Lista *L){
+    Nodo*q=malloc(sizeof(Nodo)),*p;
+    if(*L==NULL){
+        q->val=x;
+        q->sig=*L;
+        *L=q;
+    }
+    else{
+        p=*L;
+        while(p->sig!=NULL)p=p->sig;
+    q->val=x;
+    q->sig=p->sig;
+    p->sig=q;
+    }
+}
+void elimina1(int x, Lista*L){
+    Nodo*q=*L,*p;
+    if(q!=NULL){
+       if(q->val==x){
+        p=q;
+        q=q->sig;
+        free(p);
+       }
+       else while(q!=NULL&&(q->val)!=x){
+        p=q;
+        q=q->sig;
+       }
+       if(q!=NULL){
+        p->sig=q->sig;
+        free(q);
+       }
+
+    }
+}
+void copia(Lista L){
+    Lista k=NewList();
+    Nodo*q=malloc(sizeof(Nodo));
+    k=q;
+    if(L!=NULL){
+        while(L->sig!=NULL){
+            q->val=L->val;
+            q->sig=malloc(sizeof(Nodo));
+            q=q->sig;
+            L=L->sig;
+        }
+        q->val=L->val;
+        q->sig=NULL;
+        }
+     escribeLista("Lista2",k);
+}
+int sizeR(Lista L){
+if(L == NULL) return 0;
+else return 1+ sizeR(L->sig);
+}
+int estaEnR(int x, Lista L){
+    if(L==NULL||L->val==x)return L!=NULL;
+    else estaEnR(x,L->sig);
+}
+void insertTR(int x,Lista *L){
+    if(*L==NULL){
+        Nodo*q=malloc(sizeof(Nodo));
+        q->val=x;
+        q->sig=NULL;
+        *L=q;
+    }
+    else insertTR(x,&(*L)->sig);
+}
+void eliminaR(int x, Lista *L){
+if (*L != NULL){
+if ((*L)->val == x) {
+Nodo *p = *L;
+*L = (*L)->sig;
+free(p);
+}
+else eliminaR(x, &((*L)->sig));
+}
+}
+int sizeI(Lista L){
+    int k=0;
+    while(L!=NULL){
+        L=L->sig;
+        k++;
+    }
+    return k;
+}
+int maxR(Lista L){
+    if(L->sig!=NULL){
+       Nodo*q=L;
+       if(q->val>=(q->sig)->val){
+            q->sig=(q->sig)->sig;
+            maxR(&*q);
+       }
+       else maxR(L->sig);
+
+    }
+    else return L->val;
+}
+void main(){
+    int op,m,opR;
+    Lista l=NewList();
+    char name[6]="Lista";
+    int x;
+    do{
+            printf("\nSeleccionar metodo a utilizar.\n1)Iteratico\n2)Recursivo\n3)Salir\n");
+            scanf("%d",&m);
+            switch(m){
+                case 1:{
+    printf("\nMarque con un numero la opcion deseada:\n1)Eliminar valor\n2)Ingresar un valor en la cabeza de la lista \n3)Verificar si es vacia la lista \n4)Primer valor de la lista \n5)Buscar valor en la lista \n6)Imprimir lista \n7)Insertar valor al final\n8)Copiar lista\n9)Tamaño de la lista\n");
+    scanf("%d",&op);
+    switch(op){
+        case 1:{
+            printf("\nIngrese valor a eliminar:");
+            scanf("%d",&x);
+            elimina1(x,&l);
+            break;
+        }
+        case 2:{
+            printf("\nIngrese valor:");
+            scanf("%d",&x);
+            insertCab(x,&l);
+            break;
+        }
+        case 3:{
+            if(esVacia(l)==1)printf("\nLa lista si es vacia");
+            else printf("\nNo es vacia");
+            break;
+        }
+        case 4:{
+            printf("\nEl primer valor de la lista es %d",firstInList(l));
+            break;
+        }
+        case 5:{
+            printf("\nIngrese el numero que busca:");
+            scanf("%d",&x);
+            if(estaEn(x,l)==1)printf("\nEl numero %d, si se encuentra en la lista",x);
+            else printf("\nEl numero  %d no se encuentra",x);
+            break;
+        }
+        case 6:{
+            escribeLista(name,l);
+            break;
+        }
+        case 7:{
+            printf("\nIngrese valor:");
+            scanf("%d",&x);
+            insertT(x,&l);
+            break;
+        }
+        case 8:{
+            copia(l);
+            break;
+        }
+        case 9:{
+            printf("La lista tiene un tamaño de %d",sizeI(l));
+            break;
+        }
+
+    }
+    break;
+                }
+            case 2:{
+                printf("\nMarque con un numero la opcion deseada:\n1)Tamaño de lista\n2)Buscar elemento\n3)Insertar en cola\n4)Eliminar elemanto\n5)Hallar el maximo en la lista\n");
+                scanf("%d",&opR);
+                switch(opR){
+                    case 1:{
+                    printf("\nEl tamaño de la lista es %d",sizeR(l));
+                    break;
+                }
+                    case 2:{
+                        printf("\nIngrese el numero a buscar:");
+                        scanf("%d",&x);
+                        if(estaEnR(x,l)==1)printf("\nEl numero %d si se encuentra en la lista",x);
+                        else printf("\nNo se encontro en la lista");
+                        break;
+                    }
+                    case 3:{
+                        printf("Ingrese el numero a insertar:");
+                        scanf("%d",&x);
+                        insertTR(x,&l);
+                        break;
+                    }
+                    case 4:{
+                        printf("Ingrese el numero a eliminar:");
+                        scanf("%d",&x);
+                        eliminaR(x,&l);
+                        break;
+                    }
+                    case 5:{
+                        printf("El maximo de la lista es %d",maxR(l));
+                        break;
+                    }
+                break;
+            }
+    }
+    }
+    }
+            while(m!=3);
+}
--- Laboratorio 3/lab3a.c
+++ Laboratorio 3/lab3a.c
@@ -13,6 +13,10 @@ void swap(int *a, int *b){
 int t = *a; *a = *b; *b = t;
 }
 /* Versiones del algoritmo de inserción */
+
+void call(int a[], int n, void (*ord)(int *, int)){
+ord(a,n);
+}
 void insertSort(int a[], int n){
 int k = 1, j;
 while (k<n){
@@ -22,14 +26,85 @@ if (a[0] > a[1]) swap(&a[0], &a[1]);
 k++;
 }
 }
-void call(int a[], int n, void (*ord)(int *, int)){
-ord(a,n);
+//MUY CRACK EL PROFEEEE
+void ordenarMarco(int a[],int n){
+    int i=0, j;
+    while(i<n){
+            j=i+1;
+            while(j<=n){
+                 if(a[i]>a[j])swap(&a[i],&a[j]);
+                j++;
+            }
+    i++;
+    }
 }
+void insertSort21(int a[], int n){
+int k = 1, j;
+while (k<n){
+j = k;
+while (j > 0 && a[j-1] > a[j]) {swap(&a[j-1], &a[j]); j--;}
+k++;
+}
+}
+void insertSortM(int a[], int n){
+int k = 1, j, aux;
+   while (k<n){
+       j = k; aux = a[k];
+       while (j>=1 && a[j-1]>aux) {a[j] = a[j-1]; j--;}
+       a[j] = aux;
+       k++;
+       }
+}
+
+void selectionSort(int a[], int n){
+int i, j, pm, aux;
+for (i = 0; i < n-1; i++){
+pm = i; aux = a[i];
+for(j = i+1; j<n; j++)
+if (a[j] < aux){pm = j; aux = a[pm];}
+{a[pm] = a[i]; a[i] = aux;}
+}
+}
+void selectionSortY(int a[], int n){
+int i, j, pm, aux;
+for (i = 0; i < n-1; i++){
+pm = i;
+for(j = i+1; j<n; j++) if (a[j] < a[pm]) pm = j;
+if (i != pm) {aux = a[pm]; a[pm] = a[i]; a[i] = aux;}
+}
+}
+void intercambio(int a[], int n){
+int i, j;
+for (i = 0; i < n-1; i++)
+for (j = i+1; j < n; j++)
+if (a[j] < a[i]) swap(&a[i], &a[j]);
+}
+void bubbleSort(int a[], int n){
+int i, j, x;
+for (i = 1; i < n; i++)
+for(j = n-1; j >= i; j--)
+if(a[j-1] > a[j]){
+x = a[j-1]; a[j-1] = a[j]; a[j] = x;
+}
+}
+void shakeSort(int a[], int n){
+int j,k, L, R;
+L = 1, R = n-1, k = n-1;
+do{
+for (j= R; j>= L; j--)
+if(a[j-1] > a[j]){swap(&a[j-1], &a[j]); k=j;}
+L = k+1;
+for (j= L; j<= R; j++)
+if(a[j-1] > a[j]){swap(&a[j-1], &a[j]); k=j;}
+R = k-1;
+}while (L <= R);
+}
+
 int main(){
 printf("\n Probando Ordenar:\n");
 int a[] = {12, 7, 4, 9, 2, 6};
 escribeArreglo(a, "a", 6);
-call(a, 6, insertSort);
+call(a, 6, shakeSort);
 escribeArreglo(a, "a", 6);
 return 0;
 }
