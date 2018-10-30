d8f3b2108b4591f535a53da6a9d189d63219fb1a
Marcodt21 <42016727+Marcodt21@users.noreply.github.com>
2018-10-30 11:47:54 -0400

    Ordenado

--- Prueba.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main(){
-    int x;
-    void *a;
-    x=*(int*)a;
-    printf("%d",x);
-}
--- Untitled2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int *b,a;
-    *b=6;
-    a=*b;
-    printf(" uyfu *b=%d",a);
-}
